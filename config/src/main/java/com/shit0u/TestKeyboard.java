80c325bcd7d68fbfd3120a7355916bb5ec922389
shitou <lemond.wu@outlook.com>
2016-10-16 12:05:51 +0800

    [shitou] - update

---
+++ config/src/main/java/com/shit0u/TestKeyboard.java
@@ -0,0 +1,18 @@
+package com.shit0u;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author shit0u
+ * @version 2.0
+ *          Date: 16/5/11
+ *          Time: 23:23
+ * @description
+ */
+public class TestKeyboard {
+
+    public static void main(String[] args) {
+        System.out.println("This is the new keyboard");
+    }
+}
